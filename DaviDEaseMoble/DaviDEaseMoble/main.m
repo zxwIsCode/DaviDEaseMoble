7449d3d4384ac050679f7f5922b67e27eb2bed93
张小伟 <1824496534@qq.com>
2017-01-04 15:46:27 +0800

    初始化项目

---
+++ DaviDEaseMoble/DaviDEaseMoble/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DaviDEaseMoble
+//
+//  Created by 李保东 on 17/1/4.
+//  Copyright © 2017年 DaviD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
